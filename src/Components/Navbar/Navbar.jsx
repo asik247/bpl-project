beab9c3b7d0be076f8ccc5414dc04ad20ddbe666
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-02 17:24:24 +0600

    Navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import '/src/index.css'
+import logo from '/src/assets/logo.png'
 
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar fixWids">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -11,38 +13,27 @@ const Navbar = () => {
                         </div>
                         <ul
                             tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-gray">
+                            <li><a>Home</a></li>
+                            <li><a>Fixture</a></li>
+                            <li><a>Teams</a></li>
+                            <li><a>Schedules</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="text-xl"><img src={logo} alt="logo" width={70} /></a>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2 bg-base-100 w-40 z-1">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-gray ">
+                        <li><a>Home</a></li>
+                        <li><a>Fixture</a></li>
+                        <li><a>Teams</a></li>
+                        <li><a>Schedules</a></li>
                     </ul>
+                     <a className="btn rounded-xl"><span>0</span> Coin <img src="/src/assets/coin.png" alt="" /></a>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                {/* <div className="navbar-end">
+                   
+                </div> */}
             </div>
         </div>
     );
