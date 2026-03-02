a5a87cbd145ba7a7267a2fcfa7687a4e65f14e3c
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-02 23:41:17 +0600

    done toggling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Players from './Components/Players/Players';
+import Sellecteds from './Components/Sellecteds/Sellecteds';
 
 // playerPromise code start here;
 const fatchPromise = fetch("/players.json")
 .then(res=>res.json())
 console.log(fatchPromise);
 
+// useSate code here now;
+
 const App = () => {
+const [toggle,setToggle] = useState(true)
+
+// handle availabe code here;
+const handleAvailabe = ()=>{
+  console.log("handle bbtn clickdedddd");
+  setToggle(true)
+}
+const handleSellected = ()=>{
+  console.log("handle bbtn sellected");
+  setToggle(false)
+}
   return (
     <div>
      <Navbar></Navbar>
      {/* Avaiable code start here */}
     <div className='flex justify-between items-center w-11/12 mx-auto m-10'>
-      <h1>Available Players</h1>
+      {/* <h1 className='font-bold text-2xl'>Available Players</h1> */}
+      <h1>{toggle ? "Available Players" : "Selected Players"}</h1>
       <div>
-        <button className='btn border-r-0 bg-yellow-400 rounded-l-2xl'>Available</button>
-        <button className='btn border-l-0 bg-yellow-400 rounded-r-2xl'>Sellected <span>(0)</span></button>
+        <button onClick={handleAvailabe} className={`btn border-r-0 rounded-l-2xl ${toggle===true?"bg-yellow-400" :""}`}>Available</button>
+        <button onClick={handleSellected} className={`btn border-l-0  rounded-r-2xl ${toggle===false?"bg-yellow-400":""}`}>Sellected <span>(0)</span></button>
       </div>
     </div>
 
 
 
 
+    {/* Toggle implement code start here */}
 
-     {/* Players code here */}
-     <Suspense fallback={<h1>Loadding...</h1>}>
+
+    {
+      toggle === true ?<Suspense fallback={<h1>Loadding...</h1>}>
       <Players fatchPromise={fatchPromise}></Players>
      
-     </Suspense>
+     </Suspense>: <Sellecteds></Sellecteds>
+    }
+
+     {/* Players code here */}
+     
+
+     {/* Sellected page code hre nw */}
+
+
 
     </div>
   );
---
+++ src/Components/Sellecteds/Sellecteds.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sellecteds = () => {
+    return (
+        <div className='w-11/12 mx-auto'>
+            {/* <h1 className='font-bold text-2xl'>Sellected Page here;</h1> */}
+        </div>
+    );
+};
+
+export default Sellecteds;
