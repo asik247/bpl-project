7bbc8cd1056df29b831d095a77d9624bcd00399f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-03 23:28:49 +0600

    Togglig code restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Players from './Components/Players/Players';
 import Sellecteds from './Components/Sellecteds/Sellecteds';
@@ -11,45 +11,20 @@ console.log(fatchPromise);
 // useSate code here now;
 
 const App = () => {
-const [toggle,setToggle] = useState(true)
-
-// handle availabe code here;
-const handleAvailabe = ()=>{
-  console.log("handle bbtn clickdedddd");
-  setToggle(true)
-}
-const handleSellected = ()=>{
-  console.log("handle bbtn sellected");
-  setToggle(false)
-}
-
-// Choose player code start here;
-
- const handleChoosePlayer = (player) =>{
-        console.log("Chooce btn clicked",player);
-    }
 
 
   return (
     <div>
      <Navbar></Navbar>
-     {/* Avaiable code start here */}
-    <div className='flex justify-between items-center w-11/12 mx-auto m-10'>
-    {/* ✅✅✅✅✅ */}
-      <h1 className='font-bold text-2xl'>{toggle ? "Available Players" : "Selected Players"}</h1>
-      <div>
-        <button onClick={handleAvailabe} className={`btn border-r-0 rounded-l-2xl ${toggle===true?"bg-yellow-400" :""}`}>Available</button>
-        <button onClick={handleSellected} className={`btn border-l-0  rounded-r-2xl ${toggle===false?"bg-yellow-400":""}`}>Sellected <span>(0)</span></button>
-      </div>
-    </div>
-    {
-      toggle === true ?<Suspense fallback={<h1>Loadding...</h1>}>
-      <Players handleChoosePlayer={handleChoosePlayer} fatchPromise={fatchPromise}></Players>
+    
+
+    <Suspense fallback={<h1>Loadding...</h1>}>
+      <Players fatchPromise={fatchPromise}></Players>
      
-     </Suspense>: <Sellecteds></Sellecteds>
-    }
+     </Suspense>
+     <Sellecteds></Sellecteds>
     </div>
   );
-};
 
+}
 export default App;
