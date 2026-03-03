0c7a2c0f975e04754546a97a3fb5ec2416e5f3af
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-03 23:48:19 +0600

    dynamicly style added btn

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,46 @@ import Sellecteds from './Components/Sellecteds/Sellecteds';
 
 // playerPromise code start here;
 const fatchPromise = fetch("/players.json")
-.then(res=>res.json())
+  .then(res => res.json())
 console.log(fatchPromise);
 
 // useSate code here now;
 
 const App = () => {
-  const [toggle,setToggle] = useState(true);
+  const [toggle, setToggle] = useState(true);
 
 
   return (
     <div>
-     <Navbar></Navbar>
+      <Navbar></Navbar>
       <div className='flex justify-between items-center w-11/12 mx-auto mt-5'>
         <div>
-          <h1>{toggle===true?"Available Players":"Sellected Players"}</h1>
+          <h1>{toggle === true ? "Available Players" : "Sellected Players"}</h1>
         </div>
-        <div>
-          <button onClick={()=>setToggle(true)} className='btn bg-amber-300 border-r-0 rounded-l-2xl'>Available</button>
-          <button onClick={()=>setToggle(false)} className='btn bg-amber-300 border-l-0 rounded-r-2xl'>Sellected</button>
+        <div className="flex">
+          <button
+            onClick={() => setToggle(true)}
+            className={`btn ${toggle ? 'bg-amber-300' : 'bg-gray-200'}`}
+          >
+            Available
+          </button>
+
+          <button
+            onClick={() => setToggle(false)}
+            className={`btn ${!toggle ? 'bg-amber-300' : 'bg-gray-200'}`}
+          >
+            Selected
+          </button>
         </div>
       </div>
       {
-        toggle===true?<Suspense fallback={<h1>Loadding...</h1>}>
-      <Players fatchPromise={fatchPromise}></Players>
-     
-     
-     </Suspense>:<Sellecteds></Sellecteds>
+        toggle === true ? <Suspense fallback={<h1>Loadding...</h1>}>
+          <Players fatchPromise={fatchPromise}></Players>
+
+
+        </Suspense> : <Sellecteds></Sellecteds>
       }
-    
+
     </div>
   );
 
