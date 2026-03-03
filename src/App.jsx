ef2a55833f3b4f4bfa5cb4b06c883f901694ffd1
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-03 23:33:04 +0600

    Availabe text and 2 btn show

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, } from 'react';
+import React, { Suspense, useState, } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Players from './Components/Players/Players';
 import Sellecteds from './Components/Sellecteds/Sellecteds';
@@ -11,12 +11,21 @@ console.log(fatchPromise);
 // useSate code here now;
 
 const App = () => {
+  const [toggle,setToggle] = useState(true);
 
 
   return (
     <div>
      <Navbar></Navbar>
-    
+      <div className='flex justify-between items-center w-11/12 mx-auto mt-5'>
+        <div>
+          <h1>Available Players</h1>
+        </div>
+        <div>
+          <button className='btn bg-amber-300 border-r-0'>Available</button>
+          <button className='btn bg-amber-300 border-l-0'>Sellected</button>
+        </div>
+      </div>
 
     <Suspense fallback={<h1>Loadding...</h1>}>
       <Players fatchPromise={fatchPromise}></Players>
